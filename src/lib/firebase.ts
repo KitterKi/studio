d6667e64ff9710af1a81fbefd03f02afae33f106
Camilo Ramirez <calfredo.ram21@gmail.com>
2025-05-22 21:15:33 +0000

    si esta localhost

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// POR FAVOR, VERIFICA DOBLEMENTE que estos valores coincidan EXACTAMENTE
+// con los de tu proyecto "stylemyroom-l25y8" en la Consola de Firebase
+// (Configuración del proyecto > General > Tus apps > Configuración de SDK).
 const firebaseConfig = {
   apiKey: "AIzaSyBEFXDkfklp3h_5nQArEBfUOL6v0P2xCps",
   authDomain: "stylemyroom-l25y8.firebaseapp.com",
   projectId: "stylemyroom-l25y8",
-  storageBucket: "stylemyroom-l25y8.appspot.com", // Corrected from firebasestorage.app
+  storageBucket: "stylemyroom-l25y8.appspot.com", // Corregido de firebasestorage.app a appspot.com
   messagingSenderId: "99144798278",
   appId: "1:99144798278:web:663bbac40d4e2bbe273a47"
 };
